49808b602a98734ad7c2f26753b5486b22ca3d9c
LeonardoDalmazzo <leo.devfullstack@gmail.com>
2026-01-10 13:15:16 -0300

    feat: add wwwroot/js/global.js

---
+++ wwwroot/js/global.js
@@ -0,0 +1,14 @@
+// wwwroot/js/global.js
+
+// Core
+import "./core/paths.js";
+
+// Components (globais)
+import "./components/NavMenu.js";
+
+// Quando criar:
+// import "./components/Footer.js";
+// import "./components/FAB-FloatingActionButton.js";
+
+// App bootstrap
+import "./app.js";
